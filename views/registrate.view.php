76b7d3cd89b40e816ed732be3bc1ec060fc84816
Jose Luis <jluisvazquez.jv@gmail.com>
2020-07-03 23:02:35 -0500

    86 creando el formulario de registro

--- views/registrate.view.php
+++ views/registrate.view.php
@@ -13,6 +13,22 @@
 	<div class="contenedor">
 		<h1 class="titulo">Registrate</h1>
 		<hr class="border">
-		
+
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="formulario" name="login">
+			<div class="form-group">
+				<i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario">
+			</div>
+			<div class="form-group">
+				<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña">
+			</div>
+			<div class="form-group">
+				<i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir Contraseña">
+				<i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+			</div>
+		</form>
+		<p class="texto-registrate">
+			¿Ya tienes una cuenta?
+			<a href="login.php">Iniciar Sesion</a>
+		</p>
 	</div>
 </body>
