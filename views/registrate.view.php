9b061c99e03c65dbe2f48f21d4711d5fb92708cb
Jose Luis <jluisvazquez.jv@gmail.com>
2020-07-03 22:11:05 -0500

    85 creando index y preparando la pagina de registro

--- views/registrate.view.php
+++ views/registrate.view.php
@@ -5,10 +5,14 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/estilos.css">
-	<title>Crea una cuenta</title>
+	<title>Registrate</title>
 </head>
 <body>
-	
+	<div class="contenedor">
+		<h1 class="titulo">Registrate</h1>
+		<hr class="border">
+		
+	</div>
 </body>
-</html>
